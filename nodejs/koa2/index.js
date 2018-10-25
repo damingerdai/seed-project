2da94ba66a971cca42390fca4622ebc522d38806
Arthur.Ming <arthur.ming@acxiom.com>
2018-10-25 13:18:36 +0800

    fix: 修复rest失效和忽略日志失败

--- nodejs/koa2/index.js
+++ nodejs/koa2/index.js
@@ -3,6 +3,7 @@ const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 const enrouten = require('./libs/enrouten');
 const advice = require('./middlewares/advice').advice;
+const rest = require('./middlewares/rest').restify;
 const configFile = require('./libs/config').getConfigFile();
 const config = require('./config/' + configFile);
 const app = new Koa();
